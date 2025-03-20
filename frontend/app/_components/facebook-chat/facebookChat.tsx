23b9c60440b07696fe5a1e62c3e7eca9a38dfe39
robwilsoncaldosa <21103830@usc.edu.ph>
2025-03-20 17:25:53 +0800

    fix(facebook-chat): update Facebook app ID for chat functionality
    The Facebook app ID was updated to ensure the chat feature works correctly with the new configuration. This change was necessary due to the previous app ID being deprecated or no longer valid.

--- frontend/app/_components/facebook-chat/facebookChat.tsx
+++ frontend/app/_components/facebook-chat/facebookChat.tsx
@@ -4,7 +4,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 
 export const FacebookChat = () => {
   return (
-    <FacebookProvider appId="1020591736607784" chatSupport>
+    <FacebookProvider appId="1337734957440570" chatSupport>
       <CustomChat pageId="102108262516675" minimized={false}/>
     </FacebookProvider>    
   );
